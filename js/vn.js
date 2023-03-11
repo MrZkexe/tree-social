8dd332f1d2cbfc03b28ff561a733d067038aea7f
Mr-Zk_exe <78140321+MrZkexe@users.noreply.github.com>
2023-03-11 13:18:39 -0300

    Update vn.js

--- js/vn.js
+++ js/vn.js
@@ -1,13 +1,14 @@
-setTimeout(() => {
-	console.log('cuzinho');
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	setTimeout(() => {
+		console.log('cuzinho');
 
-	VanillaTilt.init(document.querySelectorAll("[id='r-box']"), {
-		max: 25,
-		speed: 1000,
-		glare: true,
-		"max-glare": 0.2,
-		reverse: true,
-		scale: 1.2,
-	});
-}, 500);
-
+		VanillaTilt.init(document.querySelectorAll("[id='r-box']"), {
+			max: 25,
+			speed: 1000,
+			glare: true,
+			"max-glare": 0.2,
+			reverse: true,
+			scale: 1.2,
+		});
+	}, 500);
+}
